3943cf422bc4a35f6bd978a388053e88fac96f36
Y <hwiyul_g@daum.net>
2023-07-15 10:56:57 +0900

    Add : MVVM structure

---
+++ app/src/main/java/com/example/preferencesdatastore_pratice/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.preferencesdatastore_pratice.viewmodels
+
+class MainActivityViewModel {
+}
--- app/src/main/java/com/example/preferencesdatastore_pratice/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/preferencesdatastore_pratice/views/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.preferencesdatastore_pratice
+package com.example.preferencesdatastore_pratice.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.preferencesdatastore_pratice.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
